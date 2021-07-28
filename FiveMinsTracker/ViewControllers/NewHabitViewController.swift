fcb0c35cea870529bfb29d051606e280b107f2f7
johyeon yoon <yds03080@naver.com>
2021-07-28 14:05:45 +0900

    [#9] Add NewHabitViewController
    - 간단히 label-textfield로 구성

---
+++ FiveMinsTracker/ViewControllers/NewHabitViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NewHabitViewController.swift
+//  FiveMinsTracker
+//
+//  Created by Johyeon Yoon on 2021/07/27.
+//
+
+import UIKit
+
+class NewHabitViewController: UIViewController {
+    
+    // 새로운 습관 추가눌러서 습관이름/ 체크리스트 만들기/ 5분 내 할 수 있는 일 적기/소셜 공개여부(토클스위치)/알람기능
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
